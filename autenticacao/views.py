74ae1f0da109a64513ac038a921fe18f5b813a0a
Daniel <d7mata@gmail.com>
2024-02-28 11:34:57 -0300

    testando as requisições get POST

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    
+    elif request.method == "POST":
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        return HttpResponse(f'{usuario} e {email}')
+
+
 def logar(request):
     return render(request, 'logar.html')
