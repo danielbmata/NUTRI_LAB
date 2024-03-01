b45d319d1f2187ea6bc648aeff177b4ad55de4da
Daniel <d7mata@gmail.com>
2024-03-01 10:59:29 -0300

    criado o db dos pacientes e a função para cadastro dos pacientes

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.messages import constants
+from .models import Pacientes
 
 @login_required(login_url='/auth/logar/')
 def pacientes(request):
-    return render(request, 'pacientes.html')
+    if request.method == "GET":
+        return render(request, 'pacientes.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sexo = request.POST.get('sexo')
+        idade = request.POST.get('idade')
+        email = request.POST.get('email')
+        telefone = request.POST.get('telefone')
+        
+        if (len(nome.strip()) == 0) or (len(sexo.strip()) == 0) or (len(idade.strip()) == 0) or (len(email.strip()) == 0) or (len(telefone.strip()) == 0):
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+            return redirect('/pacientes/')
+        
+        if not idade.isnumeric():
+            messages.add_message(request, constants.ERROR, 'Digite uma idade válida')
+            return redirect('/pacientes/')
+
+        pacientes = Pacientes.objects.filter(email=email)
+
+        if pacientes.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um paciente com esse E-mail')
+            return redirect('/pacientes/')
+                    
+        return HttpResponse(f"{nome}, {sexo}, {idade}, {email}, {telefone}")
+        
